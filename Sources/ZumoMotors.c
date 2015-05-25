76177abb42f9a292a1b1446c0777e0b9a7e12bde
Edwuard99 <edidiaconescu@gmail.com>
2015-05-25 12:01:52 +0300

    Update ZumoMotors.c

--- Sources/ZumoMotors.c
+++ Sources/ZumoMotors.c
@@ -1,33 +1,46 @@
+/*#################################################################################################################################################################################
+**		Fisierul sursa ZumoSensors.c contine functiile necesare controlului motoarelor 
+**in functie de eroare.
+**	Functiile din fisier sunt decalarate  in header-ul ZumoRobot.h.
+**#################################################################################################################################################################################*/
 #include "ZumoRobot.h"
 
-void motor(int x, int y) //valorile pentru viteza sunt cele reale (65535=max)
+void motor(int left, int right) 
 {
-	//stop();
+	/*#############################################################################################################################################################################
+	**		Functia motor este functia de control al motoarelor.
+	**Aceasta functie ia ca si argument vitezele celor doua motoare(-65535 -> 65535), iar in cazul in care viteza este negativa robotul va merge inapoi.
+	**Pasii pe care ii urmeaza:
+	**	--seteaza pinii corespunzatori motoarelor ca pini de output
+	**	--daca viteza motorului din stanga/dreapta este negativa sa inverseaza directia
+	**	--scade din viteza maxima viteza data deoarece 65535 este valoarea pentru viteza 0, iar 0 este valoarea pentru viteza 65535, astfel inversand valorile pentru viteza
+	**#############################################################################################################################################################################*/
+	const int maxspd=65535;
+
 	sens_dreapta_SetOutput();
 	sens_stanga_SetOutput();
 
-	if(x>0)
+	if(left>0)
 	{
 		sens_stanga_PutVal(1);
 	}
 	else
 	{
 		sens_stanga_PutVal(0);
-		x*=-1;
+		left=-left;
 	}
-	if(y>0)
+	if(right>0)
 	{
 		sens_dreapta_PutVal(1);
 	}
 	else
 	{
 		sens_dreapta_PutVal(0);
-		y*=-1;
+		right=-right;
 	}
-	int maxspd=65535;
 
-	PWM_stanga_SetRatio16(maxspd - x);
-	PWM_dreapta_SetRatio16(maxspd - y);
+	PWM_stanga_SetRatio16(maxspd - left);
+	PWM_dreapta_SetRatio16(maxspd - right);
 	PWM_stanga_Enable();
 	PWM_dreapta_Enable();
 
@@ -35,12 +48,26 @@ void motor(int x, int y) //valorile pentru viteza sunt cele reale (65535=max)
 }
 
 void drive(int error){
-	const int maxspd=60000, mxsp=65535;
+	/*#############################################################################################################################################################################
+	**		Functia drive controleaza motoarele in functie de valoarea erorii.
+	**Argumentul functiei este eroarea.
+	**Pasii pe care ii urmeaza:
+	**	--eroarea este inmultita cu constanta k(eroarea de 500 va fi acum de 65535), pentru a ridica valoarea acesteia la nivelul necesar pentru a controla motoarele
+	**	--daca eroarea este pozitiva -> apeleaza funcita motor unde argumentul din stanga va fi viteza maxima, iar cel din stanga diferenta dintre viteza maxima si
+	**	  eroarea -> motorul va incetini sau chiar se va roti in directie opusa in functie de eroare
+	**	--daca eroarea este negativa -> apeleaza funcita motor unde argumentul din stanga va fi suma dintre viteza maxima si eroarea, iar cel din stanga viteza
+	**	  maxima -> motorul va incetini sau chiar se va roti in directie opusa in functie de eroare
+	**#############################################################################################################################################################################*/
+	const int maxspd=65535;
 	const int k=131;
 	error*=k;
+	if(error>65535)
+		error=65535;
+	if(error<-65535)
+		error=-65535;
 	if(error>0)
-		motor(mxsp, mxsp-error);
+		motor(maxspd, maxspd-error);
 	else{
-		motor(mxsp+error, mxsp);
+		motor(maxspd+error, maxspd);
 	}
 }
